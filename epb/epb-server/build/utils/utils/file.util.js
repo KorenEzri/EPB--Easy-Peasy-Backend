6a9e58a2393c4e0b9e25d7891c222942674f1260
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-06-28 18:42:18 +0300

    update backend bugs

--- epb/epb-server/build/utils/utils/file.util.js
+++ epb/epb-server/build/utils/utils/file.util.js
@@ -103,6 +103,6 @@ const addToAllowedTypes = (name) => {
 };
 exports.addToAllowedTypes = addToAllowedTypes;
 const restartServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield write("restart.json", `{"restart":"${Math.random()}"}`);
+    yield write('node_modules/@korenezri/easy-peasy-backend/epb/client/build" "nodemon index.ts" "nodemon node_modules/@korenezri/easy-peasy-backend/epb/epb-server/build/restart.json', `{"restart":"${Math.random()}"}`);
 });
 exports.restartServer = restartServer;
